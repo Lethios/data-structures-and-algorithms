2ddc318d3b23b647dc01d84b275492996e3416cb
Lethios <204111230+Lethios@users.noreply.github.com>
2026-04-03 12:33:29 +0530

    feat: add dijkstra implementation

--- algorithms/shortest_path/python/dijkstra.py
+++ algorithms/shortest_path/python/dijkstra.py
@@ -1,1 +1,27 @@
+import heapq
+
+
+def dijkstra(graph: list[list[tuple[int, int]]], source: int) -> list[int]:
+    dist: list[int] = [10**18] * len(graph)
+    dist[source] = 0
+
+    heap: list[tuple[int, int]] = [(0, source)]
+
+    while heap:
+        curr_dist, curr_node = heapq.heappop(heap)
+
+        if curr_dist > dist[curr_node]:
+            continue
+
+        for neighbor, weight in graph[curr_node]:
+            new_dist = curr_dist + weight
+
+            if new_dist < dist[neighbor]:
+                dist[neighbor] = new_dist
+                heapq.heappush(heap, (new_dist, neighbor))
+
+    return dist
+
+
+
 
